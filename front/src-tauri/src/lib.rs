49f456baadd65ae25aaf89f2e038e6f011758fa3
Tianpao <nxtianpao@163.com>
2026-03-07 13:06:18 +0800

    refactor: 移除open_url

--- front/src-tauri/src/lib.rs
+++ front/src-tauri/src/lib.rs
@@ -6,13 +6,6 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn open_url(app: tauri::AppHandle, url: &str) -> Result<(), String> {
-    app.opener()
-        .open_url(url, None::<&str>)
-        .map_err(|e| format!("Failed to open URL: {}", e))
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -20,7 +13,7 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, open_url])
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
